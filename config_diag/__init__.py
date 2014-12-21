cfce2408c6327c3fcfae54b1bad5612b649d5363
Yasser Gonzalez <yasserglez@gmail.com>
2014-12-21 18:48:00 -0500

    Write the package and get_version docstrings

--- config_diag/__init__.py
+++ config_diag/__init__.py
@@ -1,21 +1,35 @@
 # -*- coding: utf-8 -*-
 
+"""
+Constructing Adaptive Configuration Dialogs using Crowd Data
+"""
+
 import os
 import subprocess
 
 
 def get_version():
-    src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    """Get the version number.
+    
+    Try to obtain the current version string dinamically from Git using 
+    'git describe'. If that fails, fallback to an internal variable. The 
+    latter is used in checked-out source code and it is set via a custom 
+     'python setup.py sdist/build' command when the code is exported.
+    
+    Returns:
+        The version string.
+    """
+    pkg_dir = os.path.dirname(__file__)
+    src_dir = os.path.abspath(os.path.join(pkg_dir, os.pardir))
     git_dir = os.path.join(src_dir, ".git")
     try:
-        # Try to get the latest version string dynamically from Git:
-        # (latest version, commits since latest release, and commit SHA-1)
+        # Try to get the  version string dynamically from Git.
         git_args = ("git", "--work-tree", src_dir, "--git-dir", git_dir, 
                     "describe", "--tags", "--dirty")
         with open(os.devnull, "w") as devnull:
             version = subprocess.check_output(git_args, stderr=devnull).strip()
     except subprocess.CalledProcessError:
-        # Overwritten by custom 'python setup.py sdist/build' commands
+        # Overwritten by a custom 'python setup.py sdist/build' command.
         version = None
     return version
 
