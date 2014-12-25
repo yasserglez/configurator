aa7e43c8c14cc62b2a8aeb2c23b24bbc23424a11
Yasser Gonzalez <yasserglez@gmail.com>
2014-12-25 13:40:30 -0500

    FrequencyTable outline and some test

--- config_diag/freq_table/__init__.py
+++ config_diag/freq_table/__init__.py
@@ -3,3 +3,53 @@
 """
 Frequency Table and Conditional Probabilities
 """
+
+
+class FrequencyTable(object):
+    """Compute frequency counts and conditional probabilities.
+
+    A wrapper for a pandas DataFrame that provides methods to compute
+    multivariate frequency counts and conditional probabilities.
+
+    Attributes:
+        df: A pandas Dataframe.
+    """
+    
+    def __init__(self, df):
+        """Initialize a new instance.
+
+        Arguments:
+            df: A pandas DataFrame.
+        """
+        self.df = df
+
+    def freq(self, x):
+        """Count the occurence of the sample of the variables in x.
+
+        Arguments:
+            x: A dictionary mapping some variable names to their values.
+
+        Returns:
+            The number of occurences in the DataFrame of the values in x.
+        """
+
+    def joint_prob(self, x):
+        """Joint probability distribution of x.
+
+        Arguments:
+            x: A dictionary mapping some variable names to their values.
+
+        Returns:
+            Probability value in [0,1].
+        """
+
+    def cond_prob(self, x, y):
+        """Conditional probability distribution of x given y.
+
+        Arguments:
+            x: A dictionary mapping some variable names to their values.
+            y: A dictionary mapping some variable names to their values.
+        
+        Returns:
+            Conditional probability value in [0,1].
+        """
---
+++ config_diag/freq_table/tests/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import string
+
+import numpy as np
+import pandas as pd
+
+from .. import FrequencyTable
+
+
+class TestFrequencyTable:
+
+    def setup(self):
+        np.random.seed(42)
+        rows, cols = 10, 4
+        data = np.random.randint(1, 6, rows * cols).reshape(rows, cols)
+        self.df = pd.DataFrame(data, columns=list(string.letters[:cols]))
+
+    def test_init(self):
+        freq_tab = FrequencyTable(self.df)
+        pd.util.testing.assert_frame_equal(freq_tab.df, self.df)
+
+    def test_freq(self):
+        freq_tab = FrequencyTable(self.df)
+        assert freq_tab.freq({'C': 5}) == 0
+        assert freq_tab.freq({'A': 4}) == 4
+        assert freq_tab.freq({'A': 4, 'B': 5}) == 2
+        assert freq_tab.freq({'A': 4, 'C': 3, 'D': 5}) == 3
+        assert freq_tab.freq({'A': 4, 'B': 5, 'C': 3, 'D': 5}) == 1
+
+    def test_joint_prob(self):
+        freq_tab = FrequencyTable(self.df)
+        assert freq_tab.joint_prob({'C': 5}) == 0
+        assert freq_tab.joint_prob({'A': 4}) == 0.4
+        assert freq_tab.joint_prob({'A': 4, 'B': 5}) == 0.2
+        assert freq_tab.joint_prob({'A': 4, 'C': 3, 'D': 5}) == 0.3
+        assert freq_tab.joint_prob({'A': 4, 'B': 5, 'C': 3, 'D': 5}) == 0.1
+
+    def test_cond_prob(self):
+        freq_tab = FrequencyTable(self.df)
+        assert freq_tab.cond_prob({'A': 3}, {'B': 5}) == 0.4
+        assert freq_tab.cond_prob({'A': 3, 'C': 1}, {'B': 5}) == 0.2
+        assert freq_tab.cond_prob({'A': 1}, {'B': 5, 'C': 4}) == 0
+        assert freq_tab.cond_prob({'A': 3, 'B': 5}, {'C': 1, 'D': 2}) == 0.25
+
