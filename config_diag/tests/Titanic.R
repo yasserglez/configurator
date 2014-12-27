87454d0a65a7a8b1e4f0f2bf8d061bdcb5d69656
Yasser Gonzalez <yasserglez@gmail.com>
2014-12-27 16:45:59 -0500

    Use Titanic.csv to test AssociationRuleMiner

--- config_diag/tests/Titanic.R
+++ config_diag/tests/Titanic.R
@@ -1,6 +1,13 @@
 library("datasets")
 library("epitools")
+library("arules")
 
 # Write Titanic.csv in expanded form.
-Titanic.expanded <- expand.table(Titanic)
-write.csv(Titanic.expanded, "Titanic.csv", row.names = FALSE)
+#Titanic.expanded <- expand.table(Titanic)
+#write.csv(Titanic.expanded, "Titanic.csv", quote = FALSE, row.names = FALSE)
+
+# test_assoc_rules.py
+rules <- apriori(Titanic.expanded,
+                 parameter = list(minlen = 2, target = "rules",
+                                  supp = 0.5, conf = 0.95))
+inspect(rules)
--- config_diag/tests/test_assoc_rules.py
+++ config_diag/tests/test_assoc_rules.py
@@ -1,4 +1,6 @@
 
+import os
+
 import numpy as np
 
 from ..assoc_rules import AssociationRuleMiner
@@ -7,34 +9,31 @@ from ..assoc_rules import AssociationRuleMiner
 class TestAssociationRuleMiner:
 
     def setup(self):
-        # Sample dataset from http://www.jstatsoft.org/v14/i15
-        self.data = np.array([1, 1, 0, 0,
-                              0, 1, 0, 1,
-                              1, 1, 1, 0,
-                              0, 0, 1, 0]).reshape(4, 4)
+        tests_dir = os.path.abspath(os.path.dirname(__file__))
+        self.data = np.genfromtxt(os.path.join(tests_dir, "Titanic.csv"),
+                                  skip_header=1, dtype=np.dtype(str),
+                                  delimiter=",")
 
     def test_init(self):
         miner = AssociationRuleMiner(self.data)
-        assert (miner.data == self.data).all()
+        np.testing.assert_array_equal(miner.data, self.data)
 
-    def _test_mine_assoc_rules(self, algorithm):
+    def test_mine_assoc_rlues(self):
         miner = AssociationRuleMiner(self.data)
-        rules = miner.mine_assoc_rules(min_support=0.5, min_confidence=0.5,
-                                       algorithm=algorithm)
-        assert len(rules) == 2
+        rules = miner.mine_assoc_rules(min_support=0.5, min_confidence=0.95)
+        assert len(rules) == 3
         # Rule #1
-        assert rules[0].support == 0.5
-        assert rules[0].confidence == 1.0
-        assert rules[0].lhs == {0: 1}
-        assert rules[0].rhs == {1: 1}
+        assert rules[0].lhs == {1: "Male", 3: "No"}
+        assert rules[0].rhs == {2: "Adult"}
+        np.testing.assert_almost_equal(rules[0].support, 0.6038164)
+        np.testing.assert_almost_equal(rules[0].confidence, 0.9743402)
         # Rule #2
-        assert rules[1].support == 0.5
-        assert rules[1].confidence == 0.6666667
-        assert rules[1].lhs == {1: 1}
-        assert rules[1].rhs == {0: 1}
-
-    def test_mine_assoc_rules_apriori(self):
-        self._test_mine_assoc_rules('apriori')
-
-    def test_mine_assoc_rules_fpgrowth(self):
-        self._test_mine_assoc_rules('fp-growth')
+        assert rules[1].lhs == {3: "No"}
+        assert rules[1].rhs == {2: "Adult"}
+        np.testing.assert_almost_equal(rules[1].support, 0.6533394)
+        np.testing.assert_almost_equal(rules[1].confidence, 0.9651007)
+        # Rule #3
+        assert rules[2].lhs == {1: "Male"}
+        assert rules[2].rhs == {2: "Adult"}
+        np.testing.assert_almost_equal(rules[2].support, 0.7573830)
+        np.testing.assert_almost_equal(rules[2].confidence, 0.9630272)
