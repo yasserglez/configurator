4a8eeb3eb5ac635e8cf8d6a96f46ead7c611c971
Yasser Gonzalez <yasserglez@gmail.com>
2014-12-26 20:18:27 -0500

    Outline of the classes in the assoc_rules module

--- config_diag/assoc_rules.py
+++ config_diag/assoc_rules.py
@@ -1,5 +1,67 @@
-# -*- coding: utf-8 -*-
-
 """
 Association Rule Mining
 """
+
+
+class AssociationRule:
+    """An association rule mined by AssociationRuleMiner.
+
+    Attributes:
+        lhs: Left-hand-side (also called antecedent or body) of the rule.
+            A dictionary mapping variable indexes to their values.
+        rhs: Right-hand-side (also called consequent or head) of the rule.
+            A dictionary mapping variable indexes to their values.
+        support: Item set support in [0,1].
+        confidence: Confidence of the association rule in [0,1].
+    """
+
+    def __init__(self, lhs, rhs, support, confidence):
+        """Initialize a new instance.
+
+        Arguments:
+            lhs: Left-hand-side of the rule.
+            rhs: Right-hand-side of the rule.
+            support: Item set support.
+            confidence: Confidence of the association rule.
+        """
+        self.lhs = lhs
+        self.rhs = rhs
+        self.support = support
+        self.confidence = confidence
+
+
+class AssociationRuleMiner:
+    """Association rule mining.
+
+    Discover association rules in a 2-dimensional numpy array. Each
+    column is expected to represent a categorical variable and each
+    row a multi-variate observation. Two algorithms (Apriori and
+    FP-growth) are supported for frequent item set mining, both
+    provided by Christian Borgelt's PyFIM library available at
+    http://www.borgelt.net/pyfim.html.
+
+    Attributes:
+        data: A 2-dimensional numpy array.
+    """
+
+    def __init__(self, data):
+        """Initialize a new instance.
+
+        Arguments:
+            data: A 2-dimensional numpy array.
+        """
+        self.data = data
+
+    def mine_assoc_rules(self, min_support=0.1, min_confidence=0.8,
+                         algorithm='apriori'):
+        """
+        Arguments:
+            min_support: Minimum item set support in [0,1] (default: 0.1).
+            min_confidence: Minimum confidence of the association rule in [0,1]
+                (default: 0.8).
+            algorithm: Algorithm used for frequent item set mining.
+                Possible values are: 'apriori' (default) and 'fp-growth'.
+
+        Returns:
+            A list of AssociationRule instances.
+        """
