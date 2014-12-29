b2c25ce06ab0a7fee2f33aacf05acc995c8b2399
Yasser Gonzalez <yasserglez@gmail.com>
2014-12-29 02:07:01 -0500

    Validate arguments in MDP

--- config_diag/mdp.py
+++ config_diag/mdp.py
@@ -2,6 +2,8 @@
 Markov Decision Processes
 """
 
+import mdptoolbox.util
+
 
 class MDP(object):
     """Finite MDP.
@@ -12,25 +14,41 @@ class MDP(object):
         discount_factor: Discount factor in [0,1].
     """
 
-    def __init__(self, transitions, rewards, discount_factor):
+    def __init__(self, transitions, rewards, discount_factor, validate=True):
         """Initialize a new instance.
 
-        Let n denote the number of states and m the number of actions.
+        Let S denote the number of states and A the number of actions.
 
         Arguments:
             transitions: Transition probability matrices. Either a
-                (m, n, n)-shaped numpy array or a list with m elements
-                containing (possibly sparse) (n, n) matrices. An entry
+                (A, S, S)-shaped numpy array or a list with A elements
+                containing (possibly sparse) (S, S) matrices. An entry
                 (a, s, s') gives the probability of transitioning from
                 the state s into state s' with the action a.
             rewards: Reward matrices. Same shape as transitions.
                 An entry (a, s, s') gives the reward for reaching the
                 state s' from the state s by taking the action a.
             discount_factor: Discount factor in [0,1].
+            validate: Set it to True (default) if the MDP formulation
+                should be validated, False otherwise. A ValueError
+                exception will be raised if any error is found.
         """
         self.transitions = transitions
         self.rewards = rewards
         self.discount_factor = discount_factor
+        if validate:
+            self._validate()
+
+    def _validate(self):
+        if not (0 <= self.discount_factor <= 1):
+            raise ValueError("Discount factor not in [0,1]")
+        # Rely on pymdptoolbox to check the transition and reward matrices.
+        try:
+            mdptoolbox.util.check(self.transitions, self.rewards)
+        except AssertionError as error:
+            raise ValueError(str(error))
+        except mdptoolbox.util.InvalidMDPError as error:
+            raise ValueError(error.message[len("PyMDPToolbox: "):])
 
 
 class EpisodicMDP(MDP):
@@ -44,7 +62,8 @@ class EpisodicMDP(MDP):
         terminal_state: Index of the terminal state.
     """
 
-    def __init__(self, transitions, rewards, initial_state, terminal_state):
+    def __init__(self, transitions, rewards, initial_state,
+                 terminal_state, validate=True):
         """Initialize a new instance.
 
         Arguments:
@@ -52,7 +71,13 @@ class EpisodicMDP(MDP):
             rewards: Reward matrices.
             initial_state: Index of the initial state.
             terminal_state: Index of the terminal state.
+            validate: Set it to True (default) if the MDP formulation
+                should be validated, False otherwise. A ValueError
+                exception will be raised if any error is found.
         """
-        super(EpisodicMDP, self).__init__(transitions, rewards, 1.0)
+        super(EpisodicMDP, self).__init__(transitions, rewards, 1.0, validate)
         self.initial_state = initial_state
         self.terminal_state = terminal_state
+
+    def _validate(self):
+        super(EpisodicMDP, self)._validate()
