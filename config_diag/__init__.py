e7ad9f8f7be190aae02e645ad3af55b016c39d57
Yasser Gonzalez <yasserglez@gmail.com>
2014-12-31 02:57:27 -0500

    Merge rules with the same lhs in ConfigDialogBuilder

--- config_diag/__init__.py
+++ config_diag/__init__.py
@@ -4,6 +4,12 @@ Constructing Adaptive Configuration Dialogs using Crowd Data
 
 import os
 import subprocess
+from collections import defaultdict
+from functools import reduce
+
+from sortedcontainers import SortedListWithKey
+
+from .assoc_rules import AssociationRuleMiner
 
 
 def get_version():
@@ -46,25 +52,56 @@ class ConfigDialog(object):
 
 class ConfigDialogBuilder(object):
     """Adaptive configuration dialog builder.
-
-    Attributes:
-        config_sample: A sample of the configuration variables.
     """
 
-    def __init__(self, config_sample):
+    def __init__(self, config_sample=None,
+                 assoc_rule_algorithm=None,
+                 assoc_rule_min_support=None,
+                 assoc_rule_min_confidence=None):
         """Initialize a new instance.
 
         Arguments:
             config_sample: A 2-dimensional numpy array containing a
                 sample of the configuration variables.
+            assoc_rule_algorithm: Algorithm for mining the frequent item sets.
+                Possible values are: 'apriori' and 'fp-growth'.
+            assoc_rule_min_supporte: Minimum item set support in [0,1].
+            assoc_rule_min_confidence: Minimum confidence of the rules [0,1].
         """
         super().__init__()
-        self.config_sample = config_sample
+        self._config_sample = config_sample
+        self._assoc_rule_algorithm = assoc_rule_algorithm
+        self._assoc_rule_min_support = assoc_rule_min_support
+        self._assoc_rule_min_confidence = assoc_rule_min_confidence
 
     def build_dialog():
-        """Construct the adaptive configuration dialog.
+        """Construct an adaptive configuration dialog.
 
         Returns:
             An instance of a ConfigDialog subclass.
         """
         raise NotImplementedError()
+
+    def _mine_assoc_rules(self, min_support, min_confidence, algorithm):
+        # Mine the association rules.
+        miner = AssociationRuleMiner(self._config_sample)
+        rules = miner.mine_assoc_rules(self._assoc_rule_min_support,
+                                       self._assoc_rule_min_confidence,
+                                       algorithm=self._assoc_rule_algorithm)
+        # Merge rules with the same lhs. If two rules have
+        # contradictory rhs, the rule with the greatest confidence
+        # takes precedence.
+        rule_sort_key = lambda rule: rule.confidence  # by inc. confidence
+        rules_dict = defaultdict(lambda: SortedListWithKey(key=rule_sort_key))
+        for rule in rules:
+            lhs_key = hash(frozenset(rule.lhs.items()))
+            rules_dict[lhs_key].add(rule)
+        def merge_rules(r1, r2):
+            r1.support = None
+            r1.confidence = None
+            r1.rhs.update(r2.rhs)
+            return r1
+        merged_rules = [reduce(merge_rules, grouped_rules)
+                        for grouped_rules in rules_dict.values()]
+        # Return the merged rules.
+        return merged_rules
