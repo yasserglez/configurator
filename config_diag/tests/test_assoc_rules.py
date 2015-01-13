89dc4a521c8ea7af7e210debf18ac02616918179
Yasser Gonzalez <yasserglez@gmail.com>
2015-01-12 21:47:08 -0500

    Add tests for AssociationRule

--- config_diag/tests/test_assoc_rules.py
+++ config_diag/tests/test_assoc_rules.py
@@ -2,7 +2,37 @@ from numpy.testing import (assert_raises, assert_array_equal,
                            assert_almost_equal)
 
 from .examples import load_titanic
-from ..assoc_rules import AssociationRuleMiner
+from ..assoc_rules import AssociationRule, AssociationRuleMiner
+
+
+class TestAssociationRule(object):
+
+    def setup(self):
+        lhs = {1: "a", 2: "b"}
+        rhs = {3: "c", 4: "d"}
+        support = 0.5
+        confidence = 0.9
+        self.rule = AssociationRule(lhs, rhs, support, confidence)
+
+    def test_is_lhs_compatible(self):
+        assert self.rule.is_lhs_compatible({1: "a", 2: "b"})
+        assert not self.rule.is_lhs_compatible({1: "z", 2: "b"})
+
+    def test_is_rhs_compatible(self):
+        assert self.rule.is_rhs_compatible({1: "a", 2: "b"})
+        assert self.rule.is_rhs_compatible({1: "a", 2: "b", 3: "c"})
+        assert not self.rule.is_rhs_compatible({1: "a", 2: "b", 3: "z"})
+        assert not self.rule.is_rhs_compatible({1: "a", 2: "b",
+                                                3: "c", 4: "d"})
+
+    def test_is_applicable(self):
+        assert self.rule.is_applicable({1: "a", 2: "b", 3: "c"})
+        assert not self.rule.is_applicable({1: "z", 2: "b", 3: "c"})
+
+    def test_apply_rule(self):
+        before = {1: "a", 2: "b", 3: "c"}
+        after = self.rule.apply_rule(before)
+        assert after == {1: "a", 2: "b", 3: "c", 4: "d"}
 
 
 class TestAssociationRuleMiner(object):
@@ -45,4 +75,4 @@ class TestAssociationRuleMiner(object):
     def test_mine_assoc_rules_invalid(self):
         miner = AssociationRuleMiner(self.data)
         assert_raises(ValueError, miner.mine_assoc_rules,
-                                 algorithm='invalid')
+                      algorithm='invalid')
