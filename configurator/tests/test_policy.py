b29fa28fb9b2cf3c63f4a0f3bb1c19d73c9ffbc1
Yasser Gonzalez <yasserglez@gmail.com>
2015-01-22 17:55:03 -0500

    Add tests for RLConfigDialogBuilder

--- configurator/tests/test_policy.py
+++ configurator/tests/test_policy.py
@@ -2,7 +2,7 @@ import sys
 import logging
 
 from .examples import load_email_client
-from ..policy import DPConfigDialogBuilder
+from ..policy import DPConfigDialogBuilder, RLConfigDialogBuilder
 
 
 logging.basicConfig(format="%(asctime)s:%(message)s", level=logging.DEBUG)
@@ -52,3 +52,29 @@ class TestDPConfigDialogBuilder(object):
 
     def test_policy_iteration_with_optim(self):
         self._test_builder_with_optim("policy-iteration")
+
+
+class TestRLConfigDialogBuilder(object):
+
+    def setup(self):
+        print("", file=sys.stderr)  # newline before the logging output
+
+    def _test_builder(self, algorithm):
+        builder = RLConfigDialogBuilder(
+            config_sample=EMAIL_CLIENT.config_sample,
+            assoc_rule_algorithm="apriori",
+            assoc_rule_min_support=EMAIL_CLIENT.min_support,
+            assoc_rule_min_confidence=EMAIL_CLIENT.min_confidence,
+            rl_algorithm=algorithm)
+        dialog = builder.build_dialog()
+        for var_index in EMAIL_CLIENT.questions:
+            assert dialog.get_next_question() == var_index
+            dialog.set_answer(var_index, EMAIL_CLIENT.config[var_index])
+        assert dialog.is_complete()
+        assert dialog.config == EMAIL_CLIENT.config
+
+    def test_qlearning(self):
+        self._test_builder("q-learning")
+
+    def test_sarsa(self):
+        self._test_builder("sarsa")
