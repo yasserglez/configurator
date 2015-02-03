94572bb6730ea87986b73a8bd1cbbe4a169349d7
Yasser Gonzalez <yasserglez@gmail.com>
2015-02-03 16:38:02 -0500

    Remove coverage dependences

--- runcoverage.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-if __name__ == "__main__":
-    # coverage options set in .coveragerc
-    subprocess.call("coverage erase", shell=True)
-    subprocess.call("coverage run runtests.py", shell=True)
-    subprocess.call("coverage report -m", shell=True)
