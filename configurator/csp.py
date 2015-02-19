d30cf20fe0591b8a8fd7d7ed54ca06bf461dea97
Yasser Gonzalez <yasserglez@gmail.com>
2015-02-19 11:47:34 -0500

    Methods to ensure global consistency in CSP

--- configurator/csp.py
+++ configurator/csp.py
@@ -1,17 +1,26 @@
 """Constraint satisfaction problems.
 """
 
+import copy
+import pprint
+import logging
+
 from simpleai.search import (CspProblem, backtrack,
                              MOST_CONSTRAINED_VARIABLE,
                              LEAST_CONSTRAINING_VALUE)
+
+
+log = logging.getLogger(__name__)
 
 
 class CSP(object):
     """Constraint satisfaction problem.
 
     Arguments:
+
         domain: A dictionary mapping each variable name (string) to an
-            iterable with their values (integers).
+            iterable with their values (integers). All the variables
+            must be domain consistent.
         constraints: A list of tuples with two components each: i) a
             tuple with the names of the variables involved in the
             constraint, and ii) a function that checks the constraint.
@@ -28,7 +37,10 @@ class CSP(object):
     """
 
     def __init__(self, domain, constraints):
-        self.domain = domain
+        # simpleai expects a list with the domain values
+        self.domain = {var_name: list(set(var_values))
+                       for var_name, var_values in domain.items()}
+        self._variables = tuple(self.domain.keys())  # used by simpleai
         # Ensure that the constraints are normalized.
         constraint_support = set()
         for var_names, constraint_fun in constraints:
@@ -37,40 +49,80 @@ class CSP(object):
                 raise ValueError("The constraints must be normalized")
             constraint_support.add(var_names)
         self.constraints = constraints
-        self._reset_state()
+        self.reset()
 
     def solve(self):
-        """Find a solution to the constraint satisfaction problem.
+        """Find a consistent assignment of the variables.
 
         Returns:
             A dictionary with the values assigned to the variables if
             a solution was found, :obj:`None` otherwise.
         """
-        variables = tuple(self.domain.keys())
-        csp = CspProblem(variables, self.domain, self.constraints)
+        return self._backtracking_solver(self.domain)
+
+    def _backtracking_solver(self, domain):
+        # Backtracking solver maintaining arc consistency (using the
+        # AC-3 algorithm), with the minimum-remaining-values heuristic
+        # for variable selection and the least-constraining-value
+        # heuristic for value selection.
+        csp = CspProblem(self._variables, domain, self.constraints)
         solution = backtrack(csp, variable_heuristic=MOST_CONSTRAINED_VARIABLE,
                              value_heuristic=LEAST_CONSTRAINING_VALUE,
                              inference=True)
         return solution
 
     # The following are internal methods used to keep track of the
-    # assignments and ensure global consistency after each variable is
-    # set when the dialog is being used.
+    # assignments and enforce global consistency after each variable
+    # is assigned when the dialog is being used.
 
-    def _assign_variable(self, var_index, var_value):
-        pass
+    def reset(self):
+        self._assignment = {}
+        self.pruned_domain = copy.deepcopy(self.domain)
 
-    def _get_assignment(self):
-        pass
+    def get_assignment(self):
+        return self._assignment
 
-    def _ensure_consistency(self):
-        pass
+    def assign_variable(self, var_name, var_value, enforce_consistency=True):
+        if var_name in self._assignment:
+            raise ValueError("The variable is already assigned")
+        if var_value not in self.pruned_domain[var_name]:
+            raise ValueError("Invalid assignment in the current state")
+        self._assignment[var_name] = var_value
+        if enforce_consistency:
+            self.enforce_consistency()
 
-    def _reset_state(self):
-        pass
-
-    def _push_state(self):
-        pass
-
-    def _pop_state(self):
-        pass
+    def enforce_consistency(self):
+        log.debug("enforcing global consistency")
+        log.debug("initial assignment:\n%s", pprint.pformat(self._assignment))
+        log.debug("initial domain:\n%s", pprint.pformat(self.pruned_domain))
+        # Start with unary constraints.
+        for var_name, var_value in self._assignment.items():
+            self.pruned_domain[var_name] = [var_value]
+        log.debug("after enforcing unary constraints:\n%s",
+                  pprint.pformat(self.pruned_domain))
+        # Then, check that all possible answers for the next question
+        # lead to a consistent assignment.
+        base_domain = copy.deepcopy(self.pruned_domain.copy())
+        for var_name, var_values in self.pruned_domain.items():
+            if len(var_values) > 1:
+                tmp_domain = base_domain.copy()  # shallow copy is enough
+                consistent_values = []
+                for var_value in var_values:
+                    tmp_domain[var_name] = [var_value]
+                    if self._backtracking_solver(tmp_domain) is None:
+                        log.debug("invalid value %d for %r",
+                                  var_value, var_name)
+                    else:
+                        consistent_values.append(var_value)
+                # Empty the list and then refill it, this doesn't
+                # affect existing references to the list.
+                del self.pruned_domain[var_name][:]
+                self.pruned_domain[var_name].extend(consistent_values)
+        # If the domain of a variable was reduced to a single value,
+        # set it back in the assignment.
+        for var_name, var_values in self.pruned_domain.items():
+            assert len(var_values) > 0
+            if len(var_values) == 1 and var_name not in self._assignment:
+                self._assignment[var_name] = self.pruned_domain[var_name][0]
+        log.debug("final assignment:\n%s", pprint.pformat(self._assignment))
+        log.debug("final domain:\n%s", pprint.pformat(self.pruned_domain))
--- tests/test_csp.py
+++ tests/test_csp.py
@@ -3,10 +3,11 @@ import pytest
 from configurator.csp import CSP
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def australia():
     # Chapter 5 of Russell, S. J. and Norvig, P.,
     # Artificial Intelligence: A Modern Approach, 2nd edition.
+    # http://aima.cs.berkeley.edu/2nd-ed/newchap05.pdf
     regions = ("WA", "NT", "SA", "Q", "NSW", "V", "T")
     domain = {region: [0, 1, 2] for region in regions}
     must_have_distinct_colors = lambda _, color: color[0] != color[1]
@@ -34,3 +35,38 @@ class TestCSP(object):
         for var_names, constrain_fun in australia.constraints:
             var_values = [solution[var_name] for var_name in var_names]
             assert constrain_fun(var_names, var_values)
+
+    def test_assign_variable(self, australia):
+        australia.assign_variable("T", 0, enforce_consistency=False)
+        assert australia.get_assignment() == {"T": 0}
+
+    def test_enforce_consistency(self, australia):
+        # Using the consistent assignment on page 138.
+        australia.assign_variable("T", 0)
+        assert australia.pruned_domain["WA"] == [0, 1, 2]
+        assert australia.pruned_domain["NT"] == [0, 1, 2]
+        assert australia.pruned_domain["SA"] == [0, 1, 2]
+        assert australia.pruned_domain["Q"] == [0, 1, 2]
+        assert australia.pruned_domain["NSW"] == [0, 1, 2]
+        assert australia.pruned_domain["V"] == [0, 1, 2]
+        assert australia.pruned_domain["T"] == [0]
+        assert australia.get_assignment() == {"T": 0}
+        australia.assign_variable("WA", 0)
+        assert australia.pruned_domain["WA"] == [0]
+        assert australia.pruned_domain["NT"] == [1, 2]
+        assert australia.pruned_domain["SA"] == [1, 2]
+        assert australia.pruned_domain["Q"] == [0]
+        assert australia.pruned_domain["NSW"] == [1, 2]
+        assert australia.pruned_domain["V"] == [0]
+        assert australia.pruned_domain["T"] == [0]
+        assert australia.get_assignment() == {"WA": 0, "Q": 0, "V": 0, "T": 0}
+        australia.assign_variable("NT", 1)
+        assert australia.pruned_domain["WA"] == [0]
+        assert australia.pruned_domain["NT"] == [1]
+        assert australia.pruned_domain["SA"] == [2]
+        assert australia.pruned_domain["Q"] == [0]
+        assert australia.pruned_domain["NSW"] == [1]
+        assert australia.pruned_domain["V"] == [0]
+        assert australia.pruned_domain["T"] == [0]
+        assert australia.get_assignment() == {"WA": 0, "NT": 1, "SA": 2,
+                                              "Q": 0, "NSW": 1, "V": 0, "T": 0}
