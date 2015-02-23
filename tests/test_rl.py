2c3ee6730082d42da14330d33c53bb267e323a67
Yasser Gonzalez <yasserglez@gmail.com>
2015-02-23 14:12:52 -0500

    Test RLDialogBuilder based on CSP

--- tests/test_rl.py
+++ tests/test_rl.py
@@ -1,22 +1,23 @@
 from configurator.rl import RLDialogBuilder
 
 
-class TestRLDialogBuilder(object):
+class BaseTestRLDialogBuilder(object):
+
+    def _create_builder(self, algorithm, table, table_features, email_client):
+        raise NotImplementedError
 
     def _test_builder(self, algorithm, table, table_features, email_client):
-        builder = RLDialogBuilder(domain=email_client.domain,
-                                  rules=email_client.rules,
-                                  sample=email_client.sample,
-                                  rl_algorithm=algorithm,
-                                  rl_table=table,
-                                  rl_table_features=table_features,
-                                  validate=True)
+        builder = self._create_builder(algorithm,
+                                       table, table_features,
+                                       email_client)
         dialog = builder.build_dialog()
-        for var_index in email_client.questions:
-            assert dialog.get_next_question() == var_index
-            dialog.set_answer(var_index, email_client.config[var_index])
-        assert dialog.is_complete()
-        assert dialog.config == email_client.config
+        for config, num_questions in email_client.scenarios:
+            dialog.reset()
+            for i in range(num_questions):
+                var_index = dialog.get_next_question()
+                dialog.set_answer(var_index, config[var_index])
+            assert dialog.is_complete()
+            assert dialog.config == config
 
     def test_qlearning_exact(self, email_client):
         self._test_builder("q-learning", "exact", None, email_client)
@@ -51,3 +52,27 @@ class TestRLDialogBuilder(object):
         self._test_approx_builder("sarsa",
                                   ["known-variables", "last-question"],
                                   email_client)
+
+
+class TestRuleRLDialogBuilder(BaseTestRLDialogBuilder):
+
+    def _create_builder(self, algorithm, table, table_features, email_client):
+        return RLDialogBuilder(domain=email_client.domain,
+                               rules=email_client.rules,
+                               sample=email_client.sample,
+                               rl_algorithm=algorithm,
+                               rl_table=table,
+                               rl_table_features=table_features,
+                               validate=True)
+
+
+class TestCSPRLDialogBuilder(BaseTestRLDialogBuilder):
+
+    def _create_builder(self, algorithm, table, table_features, email_client):
+        return RLDialogBuilder(domain=email_client.domain,
+                               constraints=email_client.constraints,
+                               sample=email_client.sample,
+                               rl_algorithm=algorithm,
+                               rl_table=table,
+                               rl_table_features=table_features,
+                               validate=True)
