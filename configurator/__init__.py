9fd067216aab9fca555e141a7910a4469ade2b6d
Yasser Gonzalez <yasserglez@gmail.com>
2015-02-23 23:59:56 -0500

    Fix errors on Travis CI

--- configurator/__init__.py
+++ configurator/__init__.py
@@ -13,11 +13,10 @@ def _get_version(version=None):  # overwritten by setup.py
         pkg_dir = os.path.dirname(__file__)
         src_dir = os.path.abspath(os.path.join(pkg_dir, os.pardir))
         git_dir = os.path.join(src_dir, ".git")
-        git_args = ("git", "--work-tree", src_dir, "--git-dir", git_dir,
-                    "describe", "--tags", "--dirty")
-        with open(os.devnull, "w") as devnull:
-            output = subprocess.check_output(git_args, stderr=devnull)
-            version = output.decode("utf-8").strip()
+        git_args = ("git", "--work-tree", src_dir, "--git-dir",
+                    git_dir, "describe", "--tags", "--dirty")
+        output = subprocess.check_output(git_args)
+        version = output.decode("utf-8").strip()
     return version
 
 __version__ = _get_version()
