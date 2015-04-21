b2d0cbe2f2d694d8af5412b5e063a1b399bca263
Yasser Gonzalez <yasserglez@gmail.com>
2015-04-21 00:36:35 -0400

    RLDialog serialization

--- tests/test_rl.py
+++ tests/test_rl.py
@@ -1,6 +1,38 @@
 import pytest
 
+from configurator.dialogs import Dialog
 from configurator.policy.rl import RLDialogBuilder
+
+
+class TestRLDialog(object):
+
+    @pytest.mark.parametrize(("use_rules", "table"),
+                             ((True, "exact"),
+                              (True, "approx"),
+                              (False, "exact"),
+                              (False, "approx")),
+                             ids=("rules-exact",
+                                  "rules-approx",
+                                  "constraints-exact",
+                                  "constraints-approx"))
+    def test_save(self, tmpdir, use_rules, table, email_client):
+        rules = email_client.rules if use_rules else None
+        constraints = None if use_rules else email_client.constraints
+        builder = RLDialogBuilder(email_client.var_domains,
+                                  email_client.sample,
+                                  rules=rules,
+                                  constraints=constraints,
+                                  total_episodes=50,
+                                  table=table)
+        saved_dialog = builder.build_dialog()
+        file_path = str(tmpdir.join("dialog.zip"))
+        saved_dialog.save(file_path)
+        new_dialog = Dialog.load(file_path)
+        assert new_dialog.var_domains == saved_dialog.var_domains
+        new_dialog.reset()
+        assert new_dialog.get_next_question() == 1
+        new_dialog.set_answer(1, "lgi")
+        assert new_dialog.config == {0: "no", 1: "lgi"}
 
 
 class _TestRLDialogBuilder(object):
