7e356bf240603c3410b4d0b0ff48b69ef05e0696
Yasser Gonzalez <yasserglez@gmail.com>
2015-07-14 10:58:05 -0400

    Rename SequenceDialog into GADialog

--- configurator/sequence/ga.py
+++ configurator/sequence/ga.py
@@ -12,11 +12,16 @@
 """
 
 import logging
+import random
+import pprint
+import collections
+import zipfile
 
+import dill
 import numpy as np
 from pyeasyga.pyeasyga import GeneticAlgorithm
 
-from . import SequenceDialogBuilder, SequenceDialog
+from ..dialogs import DialogBuilder, Dialog
 
 
 __all__ = ["GADialogBuilder"]
@@ -25,7 +30,7 @@ __all__ = ["GADialogBuilder"]
 log = logging.getLogger(__name__)
 
 
-class GADialogBuilder(SequenceDialogBuilder):
+class GADialogBuilder(DialogBuilder):
     """Build a configuration dialog using genetic algorithms.
 
     Arguments:
@@ -59,9 +64,15 @@ class GADialogBuilder(SequenceDialogBuilder):
                  mutation_prob=0.2,
                  tournament_size=2,
                  validate=False):
-        super().__init__(var_domains, sample, rules, constraints,
-                         total_episodes, consistency, eval_episodes,
-                         initial_solution, validate)
+        super().__init__(var_domains, sample, rules, constraints, validate)
+        if consistency not in {"global", "local"}:
+            raise ValueError("Invalid consistency value")
+        if initial_solution not in {"random", "degree"}:
+            raise ValueError("Invalid initial_solution value")
+        self._total_episodes = total_episodes
+        self._consistency = consistency
+        self._eval_episodes = eval_episodes
+        self._initial_solution = initial_solution
         self._population_size = population_size
         self._mutation_prob = mutation_prob
         self._tournament_size = tournament_size
@@ -85,14 +96,14 @@ class GADialogBuilder(SequenceDialogBuilder):
         ga.mutate_function = self._mutate_var_perm
         ga.selection_function = ga.tournament_selection
         ga.tournament_size = self._tournament_size
-        ga.fitness_function = self._fitness
+        ga.fitness_function = self._eval_var_perm
         log.info("running the GA for %d generations", num_generations)
         ga.run()
         mean_questions, var_perm = ga.best_individual()
         log.info("best average number of questions %g", mean_questions)
-        dialog = SequenceDialog(self.var_domains, var_perm,
-                                self.rules, self.constraints,
-                                validate=self._validate)
+        dialog = GADialog(self.var_domains, var_perm,
+                          self.rules, self.constraints,
+                          validate=self._validate)
         return dialog
 
     def _create_individual(self, data=None):
@@ -100,6 +111,34 @@ class GADialogBuilder(SequenceDialogBuilder):
             return self._generate_random_var_perm()
         elif self._initial_solution == "degree":
             return self._generate_degree_var_perm()
+
+    def _generate_random_var_perm(self):
+        var_perm = list(range(len(self.var_domains)))
+        random.shuffle(var_perm)
+        return var_perm
+
+    def _generate_degree_var_perm(self):
+        var2tier = collections.Counter()
+        if self.rules:
+            # Variables that appear the most in the LHS go first.
+            reverse_tiers = True
+            for rule in self.rules:
+                for var_index in rule.lhs.keys():
+                    var2tier[var_index] += 1
+        else:
+            # Variables that participate in many constraints go last.
+            reverse_tiers = False
+            for var_indices, constraint_fun in self.constraints:
+                for var_index in var_indices:
+                    var2tier[var_index] += 1
+        tier2vars = collections.defaultdict(list)
+        for var_index, tier in var2tier.items():
+            tier2vars[tier].append(var_index)
+        var_perm = []
+        for var_tier in sorted(tier2vars.keys(), reverse=reverse_tiers):
+            random.shuffle(tier2vars[var_tier])  # break ties randomly
+            var_perm.extend(tier2vars[var_tier])
+        return var_perm
 
     def _crossover(self, parent1, parent2):
         # Precedence Preservative Crossover (PPX).
@@ -113,5 +152,92 @@ class GADialogBuilder(SequenceDialogBuilder):
                 p2.remove(elem)
         return children
 
-    def _fitness(self, individual, data=None):
-        return self._eval_var_perm(individual)
+    def _mutate_var_perm(self, var_perm):
+        # Randomly swap two questions.
+        i = random.randint(0, len(var_perm) - 1)
+        j = random.randint(0, len(var_perm) - 1)
+        var_perm[i], var_perm[j] = var_perm[j], var_perm[i]
+
+    def _eval_var_perm(self, var_perm, data=None):
+        log.debug("evaluating permutation:\n%s", pprint.pformat(var_perm))
+        log.info("simulating %d episodes", self._eval_episodes)
+        dialog = GADialog(self.var_domains, var_perm,
+                          self.rules, self.constraints)
+        num_questions = []
+        for i in range(self._eval_episodes):
+            result = self._simulate_dialog(dialog)
+            if result is None:
+                log.debug("finished an episode, reached an inconsistent state")
+            else:
+                log.debug("finished an episode, asked %d questions", result)
+                num_questions.append(result)
+        mean_questions = np.mean(num_questions)
+        log.info("finished %d complete episodes, average number of questions %g",
+                 len(num_questions), mean_questions)
+        return mean_questions
+
+    def _simulate_dialog(self, dialog):
+        num_questions = 0
+        dialog.reset()
+        while dialog.is_consistent() and not dialog.is_complete():
+            var_index = dialog.get_next_question()
+            # Simulate the user response.
+            var_values = dialog.get_possible_answers(var_index)
+            probs = np.empty_like(var_values, dtype=np.float)
+            for i, var_value in enumerate(var_values):
+                response = {var_index: var_value}
+                probs[i] = self._freq_table.cond_prob(response, dialog.config)
+            bins = np.cumsum(probs / probs.sum())
+            sampled_bin = np.random.random((1, ))
+            var_value = var_values[int(np.digitize(sampled_bin, bins))]
+            # Give the answer back to the dialog.
+            dialog.set_answer(var_index, var_value, self._consistency)
+            num_questions += 1
+        return num_questions if dialog.is_consistent() else None
+
+
+class GADialog(Dialog):
+    """Configuration dialog based on permutation sequences.
+
+    Arguments:
+        var_perm: A list containing a permutation of the variables.
+
+    See :class:`configurator.dialogs.Dialog` for information about the
+    remaining arguments, attributes and methods.
+    """
+
+    def __init__(self, var_domains, var_perm,
+                 rules=None, constraints=None, validate=False):
+        self.var_perm = var_perm
+        super().__init__(var_domains, rules, constraints, validate)
+
+    def _validate(self):
+        if set(self.var_perm) != set(range(len(self.var_domains))):
+            raise ValueError("Invalid var_perm value")
+
+    def reset(self):
+        super().reset()
+        self._curr_var_index = 0
+
+    def get_next_question(self):
+        if not self.is_complete():
+            while self.var_perm[self._curr_var_index] in self.config:
+                self._curr_var_index += 1
+        next_question = self.var_perm[self._curr_var_index]
+        return next_question
+
+    def save(self, file_path):
+        with zipfile.ZipFile(file_path, "w") as zip_file:
+            zip_file.writestr("__class__", dill.dumps(self.__class__))
+            zip_file.writestr("var_domains", dill.dumps(self.var_domains))
+            zip_file.writestr("rules", dill.dumps(self.rules))
+            zip_file.writestr("constraints", dill.dumps(self.constraints))
+            zip_file.writestr("var_perm", dill.dumps(self.var_perm))
+
+    @classmethod
+    def load(cls, zip_file):
+        var_domains = dill.loads(zip_file.read("var_domains"))
+        rules = dill.loads(zip_file.read("rules"))
+        constraints = dill.loads(zip_file.read("constraints"))
+        var_perm = dill.loads(zip_file.read("var_perm"))
+        return GADialog(var_domains, var_perm, rules, constraints)
--- tests/test_ga.py
+++ tests/test_ga.py
@@ -1,6 +1,42 @@
 import pytest
 
-from configurator.sequence.ga import GADialogBuilder
+from configurator.dialogs import Dialog
+from configurator.sequence.ga import GADialogBuilder, GADialog
+
+
+class TestGADialog(object):
+
+    @pytest.mark.parametrize("use_rules", (True, False),
+                             ids=("rules", "constraints"))
+    def test_get_next_question(self, use_rules, email_client):
+        rules = email_client.rules if use_rules else None
+        constraints = None if use_rules else email_client.constraints
+        dialog = GADialog(email_client.var_domains, [1, 0],
+                          rules=rules,
+                          constraints=constraints,
+                          validate=True)
+        dialog.reset()
+        assert dialog.get_next_question() == 1
+        dialog.set_answer(1, "lgi")
+        assert dialog.is_complete()
+
+    @pytest.mark.parametrize("use_rules", (True, False),
+                             ids=("rules", "constraints"))
+    def test_save(self, tmpdir, use_rules, email_client):
+        rules = email_client.rules if use_rules else None
+        constraints = None if use_rules else email_client.constraints
+        saved_dialog = GADialog(email_client.var_domains, [1, 0],
+                                rules=rules,
+                                constraints=constraints)
+        file_path = str(tmpdir.join("dialog.zip"))
+        saved_dialog.save(file_path)
+        new_dialog = Dialog.load(file_path)
+        assert new_dialog.var_domains == saved_dialog.var_domains
+        assert new_dialog.var_perm == saved_dialog.var_perm
+        new_dialog.reset()
+        assert new_dialog.get_next_question() == 1
+        new_dialog.set_answer(1, "lgi")
+        assert new_dialog.config == {0: "no", 1: "lgi"}
 
 
 class _TestGADialogBuilder(object):
